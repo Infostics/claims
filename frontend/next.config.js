7cd2fab25aeb6d70be64c355371d55f1891d1d6e
muskankushwah5 <143391688+muskankushwah5@users.noreply.github.com>
2024-03-11 09:27:54 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,21 +3,13 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: false,
   env: {
-    // BACKEND_DOMAIN: "https://claims-backend-apis.onrender.com",
-    BACKEND_DOMAIN: "http://localhost:3006",
+    BACKEND_DOMAIN: "https://claims-backend-apis.onrender.com",
+    // BACKEND_DOMAIN: "http://localhost:3006",
     CRYPTO_SECRET_KEY: "gjfdkhslbreif847593rewfdkjbcm34woebkdjcnx43oihefdkcnx",
     COOKIE_PASSWORD: "ierfkgj439802vfckdh5438909endck",
     NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: "dcrq3m6dx",
     CLOUDINARY_CLOUD_NAME: "dcrq3m6dx",
     NEXT_PUBLIC_CLOUDINARY_API_KEY: "121866971488326",
-<<<<<<< Updated upstream
-   
-=======
-    S3_BUCKET:'',
-    REGION:'',
-    AWS_ACCESS_KEY_ID:'',
-    AWS_SECRET_ACCESS_KEY:''
->>>>>>> Stashed changes
   }
   ,
   images: {
